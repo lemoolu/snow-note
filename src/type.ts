bf190b7e3b7a8586a5bcf891c1fe082a3b160de2
lemoolu <lomo_hao@163.com>
2023-02-13 09:11:03 +0800

    add write

--- src/type.ts
+++ src/type.ts
@@ -2,6 +2,7 @@ import type { DataNode, TreeProps } from 'antd/es/tree';
 
 export interface Setting {
   root: string;
+  fileSaveDebounceWait: number;
 }
 
 export interface FileOrDir extends DataNode {
--- src/util/index.ts
+++ src/util/index.ts
@@ -8,10 +8,30 @@ export async function getSetting(): Promise<Setting> {
   }
 }
 
+function loadDir(entries: any[]) {
+  entries.forEach(e => {
+    console.log(e)
+    if (/.*\.md/.test(e.name)) {
+      e.type = 'file';
+    } else {
+      e.type = 'dir';
+    }
+    if(entries.children) {
+      loadDir(entries.children);
+    }
+  });
+  return entries;
+}
+
 export async function getCatalog() {
   const setting = await getSetting();
   const entries = await readDir(setting.root, { dir: BaseDirectory.Home, recursive: true });
-  return entries;
+  return [{
+    name: setting.root,
+    path: '',
+    type: 'dir',
+    children: loadDir(entries)
+  }];
 }
 
 export async function readFile(filePath: string) {
